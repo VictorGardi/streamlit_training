24930947e21530f9499e153cde2b0c97f4b6cf47
Victor Olsson <victor.olsson@consultant.volvo.com>
2022-01-01 18:04:03 +0100

    test

--- app.py
+++ app.py
@@ -5,17 +5,16 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+st.header('Workouts')
 
 def init_connection():
-    key_dict = json.loads(st.secrets["textkey"])
-    # Use a service account
-    cred = credentials.Certificate(key_dict)
-    firebase_admin.initialize_app(cred)
-
-    db = firestore.client()
+    if not firebase_admin._apps:
+        key_dict = json.loads(st.secrets["textkey"])
+        cred = credentials.Certificate(key_dict)
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
+    else:
+        db = firebase_admin.get_app()
     return db
 
 def get_collection(db, collection='workouts'):
@@ -27,8 +26,8 @@ def add_doc_to_collection(collection, doc):
 def get_workout_choices(collection):
     workout_choices = list()
     for doc in collection.stream():
-        if doc.type_of_workout.lower() not in workout_choices:
-            workout_choices.append(doc.type_of_workout.lower())
+        if doc.to_dict()['type_of_workout'].lower() not in workout_choices:
+            workout_choices.append(doc.to_dict()['type_of_workout'].lower())
     return workout_choices
 
 def main():
