db81ca8dac6786d83a996326d86cc3cbddd2256e
Victor Olsson <victor.olsson@consultant.volvo.com>
2022-01-01 18:11:16 +0100

    add location

--- app.py
+++ app.py
@@ -28,20 +28,26 @@ def get_workout_choices(collection):
             workout_choices.append(doc.to_dict()['type_of_workout'].lower())
     return workout_choices
 
+def get_workout_locations(collection):
+    locations = list()
+    for doc in collection.stream():
+        if doc.to_dict()['location'].lower() not in locations:
+            locations.append(doc.to_dict()['location'].lower())
+    return locations
+
 def main():
     db = init_connection()
     collection = get_collection(db)
     choice = st.sidebar.selectbox('What do you want to do today?', ['Add workout', 'statistics'])
     if choice == 'Add workout':
-        workout_choices = get_workout_choices(collection)
-        type_of_workout = st.selectbox('What type of workout did you do?', workout_choices + ['Other', 'Add new'])
+        type_of_workout = st.selectbox('What type of workout did you do?', get_workout_choices(collection) + ['Other', 'Add new'])
         if type_of_workout != 'Add new':
-            pass
+            type_of_workout = st.text_input('Add new type of workout')
+        location = st.selectbox('Where did you workout?', get_workout_locations(collection) + ['Add new'])
+        if location != 'Add new':
+            location = st.text_input('Add new location')
+        
     else:
         pass
-
-""" for doc in collection.stream():
-	st.write("The id is: ", doc.id)
-	st.write("The contents are: ", doc.to_dict()) """
 
 main()
