0fcf74a78c068dc41608669f60d1ecdd76ec494e
Victor Olsson <victor.olsson@consultant.volvo.com>
2022-01-01 17:51:28 +0100

    add type of workout

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+from re import DEBUG
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials
@@ -7,17 +9,43 @@ st.header('Hello 🌎!')
 if st.button('Balloons?'):
     st.balloons()
 
-import json
-st.write(st.secrets)
-key_dict = json.loads(st.secrets["textkey"])
+@st.cache()
+def init_connection():
+    key_dict = json.loads(st.secrets["textkey"])
+    # Use a service account
+    cred = credentials.Certificate(key_dict)
+    firebase_admin.initialize_app(cred)
 
-# Use a service account
-cred = credentials.Certificate(key_dict)
-firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    return db
 
-db = firestore.client()
+def get_collection(db, collection='workouts'):
+    return db.collection(collection)
 
-collection = doc_ref = db.collection("workouts")
-for doc in collection.stream():
+def add_doc_to_collection(collection, doc):
+    pass
+
+def get_workout_choices(collection):
+    workout_choices = list()
+    for doc in collection.stream():
+        if doc.type_of_workout.lower() not in workout_choices:
+            workout_choices.append(doc.type_of_workout.lower())
+    return workout_choices
+
+def main():
+    db = init_connection()
+    collection = get_collection(db)
+    choice = st.sidebar.selectbox('What do you want to do today?', ['Add workout', 'statistics'])
+    if choice == 'Add workout':
+        workout_choices = get_workout_choices(collection)
+        type_of_workout = st.selectbox('What type of workout did you do?', workout_choices + ['Other', 'Add new'])
+        if type_of_workout != 'Add new':
+            pass
+    else:
+        pass
+
+""" for doc in collection.stream():
 	st.write("The id is: ", doc.id)
-	st.write("The contents are: ", doc.to_dict())
+	st.write("The contents are: ", doc.to_dict()) """
+
+main()
