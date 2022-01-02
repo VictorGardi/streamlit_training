f233fbdc9bba0a21eac24df7a517a02ae419fc48
Victor Olsson <victor.olsson@consultant.volvo.com>
2022-01-02 14:02:11 +0100

    add columns

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from firebase_admin import firestore
 import datetime
 import pandas as pd
 
-st.header('Workouts')
-
 def init_connection():
     if not firebase_admin._apps:
         key_dict = json.loads(st.secrets["textkey"])
@@ -49,31 +47,40 @@ def get_workout_locations(collection):
             locations.append(doc.to_dict()['location'])
     return locations
 
-def main():
-    db = init_connection()
-    collection = get_collection(db)
+def create_metric(collection, column):
     now = datetime.datetime.now()
     one_week_ago = now - datetime.timedelta(days=7)
     two_weeks_ago = now - datetime.timedelta(days=14)
     number_of_workouts_last_week = len(get_docs_between_dates(collection, one_week_ago, now))
     number_of_workouts_two_weeks_ago = len(get_docs_between_dates(collection, two_weeks_ago, one_week_ago))
     delta = number_of_workouts_last_week - number_of_workouts_two_weeks_ago
-    st.metric('Number of workouts during the last seven days', value=number_of_workouts_last_week, delta=delta)
+    column.metric('Number of workouts during the last seven days', value=number_of_workouts_last_week, delta=delta)
+
+def main():
+    db = init_connection()
+    collection = get_collection(db)
+    column1, column2 = st.columns(2)
+    column1.header('Workouts')
+    create_metric(collection, column2)
 
     choice = st.sidebar.selectbox('What do you want to do today?', ['Add workout', 'History'])
     if choice == 'Add workout':
-        type_of_workout = st.selectbox('What type of workout did you do?', get_workout_choices(collection) + ['Other', 'Add new'])
+        col1, col2 = st.columns(2)
+        type_of_workout = col1.selectbox('What type of workout did you do?', get_workout_choices(collection) + ['Other', 'Add new'])
         if type_of_workout == 'Add new':
-            type_of_workout = st.text_input('Add new type of workout')
-        location = st.selectbox('Where did you workout?', get_workout_locations(collection) + ['Add new'])
+            type_of_workout = col1.text_input('Add new type of workout')
+
+        location = col2.selectbox('Where did you workout?', get_workout_locations(collection) + ['Add new'])
         if location == 'Add new':
-            location = st.text_input('Add new location')
-        duration = st.number_input('What was the duration of the workout?', min_value=1, max_value=1000, help='Duration of workout in minutes')
-        date = st.date_input('At which date did you workout?')
-        time = st.time_input('At what time did you workout')
+            location = col2.text_input('Add new location')
+        
+        date = col1.date_input('At which date did you workout?')
+        time = col2.time_input('At what time did you workout')
         timestamp = datetime.datetime.combine(date, time)
-        intensity = st.number_input('How intense was the workout?', min_value=1, max_value=10)
-        distance = st.number_input('What was the distance of the workout?', min_value=0, help='Distance moved during workout in meters')
+
+        duration = col1.number_input('What was the duration of the workout?', min_value=1, max_value=1000, help='Duration of workout in minutes')
+        intensity = col2.number_input('How intense was the workout?', min_value=1, max_value=10)
+        distance = col1.number_input('What was the distance of the workout?', min_value=0, help='Distance moved during workout in meters')
         doc = {
             "type_of_workout": type_of_workout,
             "location": location,
@@ -82,13 +89,13 @@ def main():
             "duration": duration,
             "distance": distance
         }
-        add_doc = st.button('Add workout')
+        add_doc = col1.button('Add workout')
         if add_doc:
             try:
                 collection.add(doc)
-                st.success('Succesfully added workout!')
+                col1.success('Succesfully added workout!')
             except Exception as e:
-                st.exception(e)
+                col1.exception(e)
             
     else:
         print_all_docs_in_collection(collection)
