769f29e3e79398806048889574f00b00f2370062
Victor Olsson <victor.olsson@consultant.volvo.com>
2022-01-02 21:04:56 +0100

    test again

--- app.py
+++ app.py
@@ -118,10 +118,12 @@ def main():
         st.write(df)
         st.write(group_by_date)
         st.bar_chart(group_by_date)
-        temp = df.groupby(['date', 'activity'])
-        st.write(temp)
+        #temp = df.groupby(['date', 'activity'])
+        #st.write(temp)
         #temp = temp.unstack(level=-1)
         #temp.fillna(0, inplace=True)
+        temp = df[['date', 'activity']]
+
         st.write(temp)
         st.bar_chart(temp)
 
